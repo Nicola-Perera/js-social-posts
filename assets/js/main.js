d9085e3c191c7a5f632ab57a249c744d86b6f2d1
Nicola-Perera <nicola.perera96@mail.com>
2021-10-25 17:18:26 +0200

    add loop to get every single value from the objects' keys

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@
     Un secondo array conterrà solo gli id dei posts a cui abbiamo dato like.
 */
 
+// lista con i post da pubblicare nella pagina
 const postsList = [
     {
         id: `vacanze`,
@@ -15,6 +16,7 @@ const postsList = [
             avatar: `foto profilo`
         },
         likes: 123,
+        myLike: 0,
         data: `25/10/21`
     },
 
@@ -27,8 +29,21 @@ const postsList = [
             avatar: `foto profilo`
         },
         likes: 789,
+        myLike: 1,
         data: `13/05/90`
     }
 ];
 
-console.log(postsList);
+// console.log(postsList);
+
+// collegamento alla lista contenitore dei post
+const postsListRef = document.getElementById('container');
+// console.log(postsListRef);
+
+for (let i = 0; i < postsList.length; i++) {
+    const post = postsList[i];
+
+    for (const key in post) {
+            console.log(post[key]);
+        }
+    }
